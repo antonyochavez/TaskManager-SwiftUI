c8e6152deafb8e3f0fe1c141041ef36eac7efcd6
j. Antonio Chavez <jesus.chavez@suramexico.com>
2023-04-08 21:28:28 -0600

    Initial Commit

---
+++ TaskManager-SwiftUI/TaskManager_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManager_SwiftUIApp.swift
+//  TaskManager-SwiftUI
+//
+//  Created by Antonio Chavez Saucedo on 08/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManager_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
